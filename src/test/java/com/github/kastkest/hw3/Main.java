321f493f1253d9225772612dcec0b459706375b2
kastkest <endimion-xi@yandex.ru>
2022-04-09 11:03:04 +0300

    Create hw3: find missed element.

---
+++ src/test/java/com/github/kastkest/hw3/Main.java
@@ -0,0 +1,21 @@
+package com.github.kastkest.hw3;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 6, 7, 8, 9, 11, 10, 12, 13, 14, 15, 16};
+
+        findMissed(array);
+
+    }
+
+    public static void findMissed(int[] array) {
+        int missed = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != (i + 1)) {
+                missed = i + 1;
+                break;
+            }
+        }
+        System.out.println(missed);
+    }
+}
