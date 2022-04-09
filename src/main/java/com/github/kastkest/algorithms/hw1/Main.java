2730f3484c39558946cd6da31e0d5437f4d026df
kastkest <endimion-xi@yandex.ru>
2022-04-09 07:26:55 +0300

    Create project for GB algorithms

---
+++ src/main/java/com/github/kastkest/algorithms/hw1/Main.java
@@ -0,0 +1,13 @@
+package com.github.kastkest.algorithms.hw1;
+
+public class Main {
+
+    private static final int SIZE=10_000;
+
+    public static void main(String[] args) {
+
+        Notebook[] notebooks = new Notebook[SIZE];
+
+    }
+
+}
